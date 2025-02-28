f3ec9505f459ffde8ec5a4adbaa1a2fe6153e89b
Eric Ponce <ponceeric458@gmail.com>
2025-02-28 10:47:53 -0800

    Add separators

--- Client/src/components/custom/Menu/Menu.tsx
+++ Client/src/components/custom/Menu/Menu.tsx
@@ -1,12 +1,15 @@
-import { Flex, For, Text } from "@chakra-ui/react";
+import { Flex, For, Text, Separator } from "@chakra-ui/react";
 
 const menuItems = new Array(5).fill("Item");
 
 const Menu = () => {
   return (
-    <Flex padding="4" gap="8" justify="center">
-      <For each={menuItems}>{(item, index) => <Text>{item}</Text>}</For>
-    </Flex>
+    <>
+      <Flex padding="4" gap="8" justify="center">
+        <For each={menuItems}>{(item, index) => <Text>{item}</Text>}</For>
+      </Flex>
+      <Separator />
+    </>
   );
 };
 
