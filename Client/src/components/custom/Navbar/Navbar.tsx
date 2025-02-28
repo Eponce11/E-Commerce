e961c904083af18ba0adcaf494fbc4916bdaa2e7
Eric Ponce <ponceeric458@gmail.com>
2025-02-27 19:09:16 -0800

    Add style to navbar

--- Client/src/components/custom/Navbar/Navbar.tsx
+++ Client/src/components/custom/Navbar/Navbar.tsx
@@ -1,23 +1,22 @@
-import {
-  Flex,
-  Grid,
-  GridItem,
-  Box,
-  Image,
-  For,
-  Text,
-  Input,
-} from "@chakra-ui/react";
+import { Flex, Box, Input, Float, Circle } from "@chakra-ui/react";
 import { InputGroup } from "@/components/ui/input-group";
-import { LuSearch } from "react-icons/lu";
+import { LuSearch, LuShoppingCart } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <Flex gap="4" padding="4">
+    <Flex gap="4" padding="4" align="center">
       <Box background="tomato" width="80px" height="60px"></Box>
       <InputGroup flex="1" startElement={<LuSearch />}>
         <Input placeholder="Username" variant="subtle" />
       </InputGroup>
+      <Box position="relative" w="30px" h="30px">
+        <LuShoppingCart size="30px" />
+        <Float>
+          <Circle size="5" bg="red" color="white">
+            3
+          </Circle>
+        </Float>
+      </Box>
     </Flex>
   );
 };
