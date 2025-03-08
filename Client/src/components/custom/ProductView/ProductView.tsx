049f8ab9f36ec88289f69be9187ba820fecf1a82
Eric Ponce <ponceeric458@gmail.com>
2025-03-07 23:21:20 -0800

    view product images

--- Client/src/components/custom/ProductView/ProductView.tsx
+++ Client/src/components/custom/ProductView/ProductView.tsx
@@ -6,12 +6,18 @@ import {
   Heading,
   Text,
   Button,
-  Image
+  Image,
+  For,
+  Flex,
 } from "@chakra-ui/react";
-import {
-  RadioCardItem,
-  RadioCardRoot,
-} from "@/components/ui/radio-card";
+import { RadioCardItem, RadioCardRoot } from "@/components/ui/radio-card";
+
+import Test1 from "../../../common/imgs/test1.jpg";
+import Test2 from "../../../common/imgs/test2.jpg";
+import Test3 from "../../../common/imgs/test3.jpg";
+import Test4 from "../../../common/imgs/test4.jpg";
+
+const productImages = [Test1, Test2, Test3, Test4];
 
 const items = [
   { value: "next", title: "Next.js" },
@@ -56,18 +62,27 @@ const ProductAddButtons = () => {
   );
 };
 
-const productImage = () => {
-
-  return <Image src="" />
-}
-
+const ProductImages = () => {
+  return (
+    <Stack>
+      <Image src={Test1} height={500} fit="cover" />
+      <Flex gap={3} justifyContent="center">
+        <For each={productImages}>
+          {(productImg, index) => (
+            <Image src={productImg} height={50} width={50} />
+          )}
+        </For>
+      </Flex>
+    </Stack>
+  );
+};
 
 const ProductView = () => {
   return (
     <Grid h="200px" templateColumns="repeat(5, 1fr)" gap={4}>
       <GridItem rowSpan={1} colSpan={3}>
         <Box background="tomato" width="100%" padding="4" color="white">
-          This is the Box 1
+          <ProductImages />
         </Box>
       </GridItem>
       <GridItem colSpan={2}>
